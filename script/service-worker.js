fe291eb60d7eeb34a839ae4097e1b4c4e3749292
zamzameir <zamzameir@gmail.com>
2018-10-09 19:34:54 +0800

    Added cache function & removed unrelated files

---
+++ script/service-worker.js
@@ -0,0 +1,34 @@
+var dataCacheName = 'IIUMCGPA';
+var cacheName = 'IIUMCGPA';
+var filesToCache = [
+    "iiumcgpa.js",
+    "iiumcgpa.php",
+    "./",
+    "./index.html",
+    "./404.html",
+    "./redirect.html",
+    "./README.md",
+    "./LICENSE.md",
+    "./_config.yml",
+    "./images/banner_color.jpg",
+    "./images/banner_white.png",
+    "./images/icon.ico",
+    "./images/logo.png",
+    "./images/ribbon.png",
+    "./style/iiumcgpa.css"];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
